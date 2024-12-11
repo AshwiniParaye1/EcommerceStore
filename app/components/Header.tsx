5f8977b307f1688ebd8644d0da5d3b924ea57d38
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-12-11 20:58:15 +0530

    added links to cart and shop pages

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -38,7 +38,7 @@ const Header = () => {
       {/* Navigation Links */}
       <div className="ml-auto flex gap-2">
         <Link
-          href="#"
+          href="/"
           className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
           prefetch={false}
         >
@@ -52,7 +52,7 @@ const Header = () => {
           About
         </Link>
         <Link
-          href="#"
+          href="/shop"
           className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
           prefetch={false}
         >
@@ -66,12 +66,14 @@ const Header = () => {
           Contact Us
         </Link>
 
-        <Button
-          className="justify-self-end px-2 py-1 text-xs"
-          variant="outline"
-        >
-          <BsCartPlus />
-        </Button>
+        <Link href="/cart">
+          <Button
+            className="justify-self-end px-2 py-1 text-xs"
+            variant="outline"
+          >
+            <BsCartPlus />
+          </Button>
+        </Link>
         <Button
           className="justify-self-end px-2 py-1 text-xs"
           variant="outline"
