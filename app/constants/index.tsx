5884f277d121382b21274916e819cfab4f04dfcd
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-12-11 20:52:01 +0530

    created constants file

---
+++ app/constants/index.tsx
@@ -0,0 +1,41 @@
+export const productLinks = [
+  {
+    url: "/assets/images/headphone5.jpg",
+    alt: "product1"
+  },
+  {
+    url: "/assets/images/headphone6.jpg",
+    alt: "product2"
+  },
+  {
+    url: "/assets/images/camera.jpg",
+    alt: "product3"
+  },
+  {
+    url: "/assets/images/camera4.jpg",
+    alt: "product4"
+  },
+  {
+    url: "/assets/images/laptop.jpg",
+    alt: "product5"
+  },
+  {
+    url: "/assets/images/speaker.jpg",
+    alt: "product6"
+  },
+  {
+    url: "/assets/images/speaker2.jpg",
+    alt: "product6"
+  }
+];
+
+export const homePageProductLinks = [
+  {
+    url: "/assets/images/headphone5.jpg",
+    alt: "product1"
+  },
+  {
+    url: "/assets/images/headphone6.jpg",
+    alt: "product2"
+  }
+];
